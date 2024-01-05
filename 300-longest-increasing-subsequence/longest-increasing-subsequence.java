c6270c1fd5d6c2f392d147e4dfc4a4bcbdba36be
Uduthamahesh77 <145954658+Uduthamahesh77@users.noreply.github.com>
2024-01-05 15:32:37 +0530

    Time: 31 ms (73.91%) | Memory: 43.8 MB (42.38%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,7 +1,6 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int temp[]=new int[nums.length];
-        int count=Integer.MIN_VALUE;
         for(int i=0;i<nums.length;i++){
             int value=0;
                 for(int j=i-1;j>=0;j--){

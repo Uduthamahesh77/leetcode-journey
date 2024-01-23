5883dc6993be558caff753c7f031acf6c67450e7
Uduthamahesh77 <145954658+Uduthamahesh77@users.noreply.github.com>
2024-01-23 12:43:53 +0530

    Time: 36 ms (23.39%) | Memory: 51.6 MB (5.14%) - LeetSync

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -1,37 +1,55 @@
+import java.util.*;
+
 class Solution {
-    public boolean hasDuplicates(String s1,String s2){
-        int arr[]=new int[26];
-        for(char ch : s1.toCharArray()){
-            if(arr[ch-'a'] > 0){
+    private Map<String, Integer> memo;
+
+    public boolean hasDuplicates(String s1, String s2) {
+        int[] arr = new int[26];
+        for (char ch : s1.toCharArray()) {
+            if (arr[ch - 'a'] > 0) {
                 return true;
             }
-            arr[ch-'a']++;
+            arr[ch - 'a']++;
         }
-        for(char ch :s2.toCharArray()){
-            if(arr[ch-'a']>0){
+        for (char ch : s2.toCharArray()) {
+            if (arr[ch - 'a'] > 0) {
                 return true;
             }
         }
         return false;
     }
-    public int solve(int i,List<String> arr, String temp,int n){
-        if(i>=n){
+
+    public int solve(int i, List<String> arr, String temp, int n) {
+        if (i >= n) {
             return temp.length();
         }
-        int include=0;
-        int exclude=0;
-        if(hasDuplicates(arr.get(i),temp)){
-            exclude=solve(i+1,arr,temp,n);
-        }else{
-            include=solve(i+1,arr,temp+arr.get(i),n);
-            exclude=solve(i+1,arr,temp,n);
+
+        String key = i + ":" + temp;
+        if (memo.containsKey(key)) {
+            return memo.get(key);
         }
-        return Math.max(include,exclude);
+
+        int include = 0;
+        int exclude = 0;
+
+        if (hasDuplicates(arr.get(i), temp)) {
+            exclude = solve(i + 1, arr, temp, n);
+        } else {
+            include = solve(i + 1, arr, temp + arr.get(i), n);
+            exclude = solve(i + 1, arr, temp, n);
+        }
+
+        int result = Math.max(include, exclude);
+        memo.put(key, result);
+
+        return result;
     }
+
     public int maxLength(List<String> arr) {
-        String temp="";
-        int n=arr.size();
-        int i=0;
-        return solve(i,arr,temp,n);
+        String temp = "";
+        int n = arr.size();
+        int i = 0;
+        memo = new HashMap<>();
+        return solve(i, arr, temp, n);
     }
 }

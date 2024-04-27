8fb45942753918194616fc9de0258e9444728350
Uduthamahesh77 <145954658+Uduthamahesh77@users.noreply.github.com>
2024-04-27 08:48:51 +0530

    Time: 11 ms (60.19%) | Memory: 45.3 MB (16.02%) - LeetSync

--- 514-freedom-trail/freedom-trail.java
+++ 514-freedom-trail/freedom-trail.java
@@ -1,21 +1,39 @@
-class Solution {
+public class Solution {
+    public int dist(int size, int p, int t) {
+        return Math.min(Math.abs(t - p), size - Math.abs(t - p));
+    }
+
     public int findRotateSteps(String ring, String key) {
-        int n = ring.length();
-        int m = key.length();
-        int[][] dp = new int[m + 1][n];
-        
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = 0; j < n; j++) {
-                dp[i][j] = Integer.MAX_VALUE;
-                for (int k = 0; k < n; k++) {
-                    if (ring.charAt(k) == key.charAt(i)) {
-                        int diff = Math.abs(j - k);
-                        int steps = Math.min(diff, n - diff);
-                        dp[i][j] = Math.min(dp[i][j], steps + dp[i + 1][k]);
-                    }
+        int[][] mp = new int[26][100];
+        int[] freq = new int[26];
+        int[][] dp = new int[100][100];
+
+        int n = ring.length(), m = key.length();
+        for (int i = 0; i < n; ++i) {
+            mp[ring.charAt(i) - 'a'][freq[ring.charAt(i) - 'a']++] = i;
+        }
+
+        for (int i = 0; i < freq[key.charAt(0) - 'a']; ++i) {
+            dp[0][mp[key.charAt(0) - 'a'][i]] = dist(n, 0, mp[key.charAt(0) - 'a'][i]) + 1;
+        }
+
+        for (int i = 1; i < m; ++i) {
+            for (int j = 0; j < freq[key.charAt(i) - 'a']; ++j) {
+                int mini = Integer.MAX_VALUE;
+                for (int k = 0; k < freq[key.charAt(i - 1) - 'a']; ++k) {
+                    mini = Math.min(mini, dp[i - 1][mp[key.charAt(i - 1) - 'a'][k]] +
+                            dist(n, mp[key.charAt(i) - 'a'][j], mp[key.charAt(i - 1) - 'a'][k]) + 1);
                 }
+
+                dp[i][mp[key.charAt(i) - 'a'][j]] = mini;
             }
         }
-        return dp[0][0] + m;
+
+        int res = Integer.MAX_VALUE;
+        for (int i = 0; i < freq[key.charAt(m - 1) - 'a']; ++i) {
+            res = Math.min(res, dp[m - 1][mp[key.charAt(m - 1) - 'a'][i]]);
+        }
+
+        return res;
     }
 }

092ede8f377c3d476a64b8b58ad06c1253ab4e6d
Uduthamahesh77 <145954658+Uduthamahesh77@users.noreply.github.com>
2024-05-02 23:44:18 +0530

    Time: 19 ms (15.31%) | Memory: 44.4 MB (91.40%) - LeetSync

--- 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.java
+++ 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.java
@@ -2,7 +2,7 @@ class Solution {
     public int findMaxK(int[] nums) {
         int max=Integer.MIN_VALUE;
         for(int i=0;i<nums.length;i++){
-            for(int j=1;j<nums.length;j++){
+            for(int j=i+1;j<nums.length;j++){
                 if(nums[i]+nums[j]==0){
                     max=Math.max(max,Math.abs(nums[i]));
                 }
